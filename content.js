6176f15a1cb644e2c318d92a27f49a7811e4798c
Cristi-Notebook <hcristy@gmail.com>
2015-05-11 00:41:34 +0300

    added template for 990.ro

---
+++ content.js
@@ -0,0 +1,21 @@
+var count = 0;
+var regList = [
+    {
+        href: 'http://sh',
+        pattern: "http:\/\/sh.st\/.*(http:.*)"
+    }
+];
+
+for (var i = 0; i < regList.length; i++) {
+    var regex = null;
+    $('a[href^="' + regList[i].href + '"]').each(function () {
+        if (regex == null) {
+            regex = new RegExp(regList[i].pattern);
+        }
+        var matches = regex.exec($(this).attr('href'));
+        if (matches.length > 1) {
+            count++;
+            $(this).attr('href', matches[1]);
+        }
+    });
+}
