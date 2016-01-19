c003f5bf85fa1cdd03901edcedf794c3ebbb9cb0
Cristi-Notebook <hcristy@gmail.com>
2016-01-19 19:33:55 +0200

    add site

--- content.js
+++ content.js
@@ -1,29 +1,32 @@
 var count = 0;
+var defaultPattern = ".*{0}.*(http:.*)"
 var regList = [
     {
         href: 'http://sh',
-        pattern: "http:\/\/sh.st\/.*(http:.*)"
+        // pattern: "http:\/\/sh.st\/.*(http:.*)"
     },
 	{
 		href: 'http://adf',
-		pattern: "http:\/\/adf.ly\/.*(http:.*)"
+		// pattern: "http:\/\/adf.ly\/.*(http:.*)"
 	},
 	{
 		href: 'https://www.spaste',
-		pattern: "https:\/\/www.spaste.com\/.*(http:.*)"
+		// pattern: "https:\/\/www.spaste.com\/.*(http:.*)"
 	},
     {
         href: 'http://linkshrink',
-        pattern: "http:\/\/linkshrink.net\/.*(http:.*)"
-    }
+        // pattern: "http:\/\/linkshrink.net\/.*(http:.*)"
+    },
+	{
+		href : "http://bc"
+	}
 ];
 
 for (var i = 0; i < regList.length; i++) {
-    var regex = null;
-    $('a[href^="' + regList[i].href + '"]').each(function () {
-        if (regex == null) {
-            regex = new RegExp(regList[i].pattern);
-        }
+    var pattern = regList[i].pattern || defaultPattern.replace("{0}", regList[i].href);
+	var regex = new RegExp(pattern);
+	
+    $('a[href*="' + regList[i].href + '"]').each(function () {
         var matches = regex.exec($(this).attr('href'));
         if (matches.length > 1) {
             count++;
